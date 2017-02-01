0a61409403df03b59156d9877323813d3fa2c9e6
Leonids Maslovs <leonids.maslovs@knowledgeprice.com>
2017-02-02 00:50:40 +0200

    first commit

---
+++ actions/app.go
@@ -0,0 +1,35 @@
+package actions
+
+import (
+	"os"
+
+	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/buffalo/middleware"
+	"github.com/leonids/test-buffalo/models"
+	"github.com/markbates/going/defaults"
+)
+
+// ENV is used to help switch settings based on where the
+// application is being run. Default is "development".
+var ENV = defaults.String(os.Getenv("GO_ENV"), "development")
+var app *buffalo.App
+
+// App is where all routes and middleware for buffalo
+// should be defined. This is the nerve center of your
+// application.
+func App() *buffalo.App {
+	if app == nil {
+		app = buffalo.Automatic(buffalo.Options{
+			Env:         ENV,
+			SessionName: "_test-buffalo_session",
+		})
+
+		app.Use(middleware.PopTransaction(models.DB))
+
+		app.GET("/", HomeHandler)
+
+		app.ServeFiles("/assets", assetsPath())
+	}
+
+	return app
+}
