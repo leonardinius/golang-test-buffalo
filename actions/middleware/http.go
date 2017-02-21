a00a809b549efb669e899fbf794991ba83d4b823
Leonids Maslovs <leonids.maslovs@knowledgeprice.com>
2017-02-22 01:18:17 +0200

    cleanup

--- actions/middleware/http.go
+++ actions/middleware/http.go
@@ -8,24 +8,24 @@ import (
 type httpMiddlewareFunc func(http.Handler) http.Handler
 
 type httpHandlerWrapper struct {
-	h     buffalo.Handler
-	c     buffalo.Context
-	error error
+	handler buffalo.Handler
+	ctx     buffalo.Context
+	error   error
 }
 
 func (mw httpHandlerWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	mw.error = mw.h(mw.c)
+	mw.error = mw.handler(mw.ctx)
 }
 
 func WrapHandler(handler httpMiddlewareFunc) buffalo.MiddlewareFunc {
 	return func(next buffalo.Handler) buffalo.Handler {
 
-		wrapper := &httpHandlerWrapper{h: next}
+		wrapper := &httpHandlerWrapper{handler: next}
 
-		return func(c buffalo.Context) error {
-			wrapper.c = c
-			current := handler(wrapper)
-			current.ServeHTTP(c.Response(), c.Request())
+		return func(context buffalo.Context) error {
+			wrapper.ctx = context
+			httpHandler := handler(wrapper)
+			httpHandler.ServeHTTP(context.Response(), context.Request())
 			return wrapper.error
 		}
 	}
